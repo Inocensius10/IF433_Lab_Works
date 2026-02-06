1d08324283a17655ee5a0d29ec8433ec0e7525a8
INO22CEN <148705377+INO22CEN@users.noreply.github.com>
2026-02-06 22:43:21 +0700

    week01: (tugas 1) clean up unused args and comments

--- src/week01/CircleCalculator.kt
+++ src/week01/CircleCalculator.kt
@@ -1,14 +1,12 @@
 package week01
 
-fun main(args: Array<String>) {
+fun main() {
     val radius = 7.0
     val pi = 3.14
     val area = pi * radius * radius
 
     println("Radius: $radius, Area: $area")
-
-    // Logic check
-    checkSize(area)
+    println(checkSize(area))
 }
 
 fun checkSize(area: Double) =
