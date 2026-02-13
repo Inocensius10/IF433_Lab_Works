796fad90bddc191e6c7246b9d3750ec6e46b9a05
INO22CEN <inocensiusdwi@gmail.com>
2026-02-13 23:41:01 +0700

    week02: (task 2) create Hero class

---
+++ src/week02/Hero.kt
@@ -0,0 +1,24 @@
+package week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String) {
+        println("$name menyerang $targetName dengan kekuatan $baseDamage!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+        println("$name menerima $damage damage. HP sekarang: $hp")
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
