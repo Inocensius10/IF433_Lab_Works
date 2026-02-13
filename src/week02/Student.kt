07621fa762b4e6d1e7a68ee680068a91ac35ed15
INO22CEN <inocensiusdwi@gmail.com>
2026-02-13 22:46:31 +0700

    week02: add secondary constructor

--- src/week02/Student.kt
+++ src/week02/Student.kt
@@ -9,9 +9,14 @@ class Student(
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
         } else {
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
+
+    constructor(name: String, nim: String) :
+            this(name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
 }
+
