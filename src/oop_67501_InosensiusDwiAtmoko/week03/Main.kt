e7b3a232178d338b6895f75568c90f118e4136e9
INO22CEN <inocensiusdwi@gmail.com>
2026-02-20 15:22:35 +0700

    week03: cleanup code

--- src/oop_67501_InosensiusDwiAtmoko/week03/Main.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week03/Main.kt
@@ -4,15 +4,11 @@ fun main() {
 
     val e = Employee("Budi")
 
-    // 1. Test Salary
     e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    // 2. Test Encapsulation
     e.increasePerformance()
-    // e.performanceRating = 5   // harus error kalau dicoba
 
-    // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
