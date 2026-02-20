1d7a739a3b5dd95998ff1cadf396427042bf932d
INO22CEN <inocensiusdwi@gmail.com>
2026-02-20 15:05:30 +0700

    week03: simulate stack overflow error in setter

--- src/oop_67501_InosensiusDwiAtmoko/week03/Employee.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week03/Employee.kt
@@ -1,3 +1,10 @@
 package oop_67501_InosensiusDwiAtmoko.week03
 
-class Employee(val name: String)
+class Employee(val name: String) {
+
+    var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            this.salary = value   // SALAH (recursive)
+        }
+}
---
+++ src/oop_67501_InosensiusDwiAtmoko/week03/Main.kt
@@ -0,0 +1,8 @@
+package oop_67501_InosensiusDwiAtmoko.week03
+
+fun main() {
+    val e = Employee("Budi")
+
+    println("--- Test Error ---")
+    e.salary = 5000
+}
