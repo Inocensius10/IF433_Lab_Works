5ef839321b1cab915a2a9ec5d93593d305dc67d6
INO22CEN <inocensiusdwi@gmail.com>
2026-02-27 15:23:08 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_67501_InosensiusDwiAtmoko/week04/Main.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week04/Main.kt
@@ -1,4 +1,21 @@
 package oop_67501_InosensiusDwiAtmoko.week04
 
-class Main {
+fun main() {
+
+    println("---- Testing Vehicle ----")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n---- Testing Car ----")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+
+    println("\n---- Testing Electric Car ----")
+    val tesla = ElectricCar("Tesla", 4, 85)
+    tesla.openTrunk()
+    tesla.honk()
+    tesla.accelerate()
 }
