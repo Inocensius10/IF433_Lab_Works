48a55859f815cff5dfa34418bb595b51305c0625
INO22CEN <inocensiusdwi@gmail.com>
2026-03-06 15:10:04 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_67501_InosensiusDwiAtmoko/week05/Main.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week05/Main.kt
@@ -28,4 +28,17 @@ fun main() {
 
         println("----------------------------")
     }
+
+    println()
+    println("=== TEST MATH HELPER ===")
+
+    val mathHelper = MathHelper()
+
+    val luasPersegi = mathHelper.hitungLuas(4)
+    val luasPersegiPanjang = mathHelper.hitungLuas(5, 3)
+    val luasLingkaran = mathHelper.hitungLuas(7.0)
+
+    println("Luas Persegi: $luasPersegi")
+    println("Luas Persegi Panjang: $luasPersegiPanjang")
+    println("Luas Lingkaran: $luasLingkaran")
 }
