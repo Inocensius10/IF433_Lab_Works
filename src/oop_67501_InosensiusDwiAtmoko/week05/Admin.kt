2e9020fe05368f4564153c0171876dcab37c0886
INO22CEN <inocensiusdwi@gmail.com>
2026-03-06 15:06:14 +0700

    week05: create Admin class inheriting Pegawai

---
+++ src/oop_67501_InosensiusDwiAtmoko/week05/Admin.kt
@@ -0,0 +1,14 @@
+package oop_67501_InosensiusDwiAtmoko.week05
+
+class Admin(
+    nama: String
+) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("$nama sedang duduk di depan komputer melayani administrasi.")
+    }
+
+    fun doAdminWork() {
+        println("$nama sedang merekap data absensi mahasiswa.")
+    }
+}
