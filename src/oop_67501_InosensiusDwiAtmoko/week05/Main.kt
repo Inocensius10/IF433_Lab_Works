d01bc1a89ce4fd753b1a4df083d640c46d894573
INO22CEN <inocensiusdwi@gmail.com>
2026-03-06 15:08:23 +0700

    week05: (task 1) create MathHelper class with overloading

--- src/oop_67501_InosensiusDwiAtmoko/week05/Main.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week05/Main.kt
@@ -1,4 +1,31 @@
 package oop_67501_InosensiusDwiAtmoko.week05
 
-class Main {
+fun main() {
+
+    val dosen1 = Dosen("Pak Alex", "0123456")
+    val admin1 = Admin("Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+
+    for (pegawai in daftarPegawai) {
+
+        pegawai.bekerja()
+
+        when (pegawai) {
+
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+
+        println("----------------------------")
+    }
 }
---
+++ src/oop_67501_InosensiusDwiAtmoko/week05/MathHelper.kt
@@ -0,0 +1,17 @@
+package oop_67501_InosensiusDwiAtmoko.week05
+
+class MathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+
+}
