06574a07236f17b603df8bae99e923d687156563
INO22CEN <inocensiusdwi@gmail.com>
2026-03-13 15:34:59 +0700

    week06: (task) test smart home system logic in main

--- src/oop_67501_InosensiusDwiAtmoko/week06/Main.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week06/Main.kt
@@ -23,6 +23,15 @@ fun main() {
     val speaker = SmartSpeaker("S1", "Google Nest Dapur")
     val cctv = SmartCCTV("C1", "Ezviz Garasi")
 
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
+
     println("\n=== TESTING CHECKOUT ===")
 
     processCheckout(pay1, 50000.0)
