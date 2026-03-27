a5706de1159e31499c93c446ddbf0df228e331e4
INO22CEN <inocensiusdwi@gmail.com>
2026-03-27 16:04:38 +0700

    week07: (task) implement factory pattern in WeaponForge

---
+++ src/oop_67501_InosensiusDwiAtmoko/week07/WeaponForge.kt
@@ -0,0 +1,27 @@
+package oop_67501_InosensiusDwiAtmoko.week07
+
+class Weapon private constructor(
+    val item: GameItem,
+    val durability: Int
+) {
+
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            val item = GameItem(
+                "Pedang Kayu Bapuk",
+                5,
+                ItemRarity.COMMON
+            )
+            return Weapon(item, 50)
+        }
+
+        fun forgeEpicSword(): Weapon {
+            val item = GameItem(
+                "Pedang Naga Legendaris",
+                100,
+                ItemRarity.EPIC
+            )
+            return Weapon(item, 200)
+        }
+    }
+}
