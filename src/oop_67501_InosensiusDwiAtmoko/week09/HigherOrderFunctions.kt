977a54e04bdea1c8cf9735d5093347fda87d2963
INO22CEN <inocensiusdwi@gmail.com>
2026-04-24 15:11:34 +0700

    week09: implement filter HOF to extract data

---
+++ src/oop_67501_InosensiusDwiAtmoko/week09/HigherOrderFunctions.kt
@@ -0,0 +1,25 @@
+package oop_67501_InosensiusDwiAtmoko.week09
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+    println("Original Data: $numbers")
+
+    println("\n=== HOF: FILTER ===")
+    val evens = numbers.filter { it % 2 == 0 }
+    println("Evens only: $evens")
+
+    println("\n=== HOF: MAP ===")
+    val multiplied = evens.map { it * 10 }
+    val asStrings = multiplied.map { "Rp ${it}k" }
+    println("Multiplied: $multiplied")
+    println("Formatted: $asStrings")
+
+    println("\n=== HOF: SORTED & FOREACH ===")
+    val randomNames = listOf("Charlie", "Alice", "Bob")
+
+    val sortedReverse = randomNames.sortedByDescending { it }
+
+    sortedReverse.forEach { name ->
+        println("User: $name")
+    }
+}
