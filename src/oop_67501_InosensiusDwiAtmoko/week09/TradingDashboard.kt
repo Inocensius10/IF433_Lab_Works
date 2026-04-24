664785c037b65d969e075cf6fedaf9d4a22cfeb4
INO22CEN <inocensiusdwi@gmail.com>
2026-04-24 15:24:10 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_67501_InosensiusDwiAtmoko/week09/TradingDashboard.kt
@@ -0,0 +1,14 @@
+package oop_67501_InosensiusDwiAtmoko.week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 5, 3.2, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 15, 7.8, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 12.1, "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 8, -2.4, "OPEN")
+    )
+
+}
