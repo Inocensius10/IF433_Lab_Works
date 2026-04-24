d9ecc63c98b367782c192737ae6ad7c029548241
INO22CEN <inocensiusdwi@gmail.com>
2026-04-24 15:18:29 +0700

    week09: implement sortedByDescending and forEach

--- src/oop_67501_InosensiusDwiAtmoko/week09/HigherOrderFunctions.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week09/HigherOrderFunctions.kt
@@ -16,9 +16,7 @@ fun main() {
 
     println("\n=== HOF: SORTED & FOREACH ===")
     val randomNames = listOf("Charlie", "Alice", "Bob")
-
     val sortedReverse = randomNames.sortedByDescending { it }
-
     sortedReverse.forEach { name ->
         println("User: $name")
 
