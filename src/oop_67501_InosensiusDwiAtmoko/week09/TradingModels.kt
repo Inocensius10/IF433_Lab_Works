6c9c86f5a621d822352e2e0d7b374e68c903f7d4
INO22CEN <inocensiusdwi@gmail.com>
2026-04-24 15:23:30 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_67501_InosensiusDwiAtmoko/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_67501_InosensiusDwiAtmoko.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
