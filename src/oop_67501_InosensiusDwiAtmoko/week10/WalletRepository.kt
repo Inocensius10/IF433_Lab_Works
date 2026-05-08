cbd92b4683da23e297b1c3e789b356623e695930
INO22CEN <inocensiusdwi@gmail.com>
2026-05-08 13:40:14 +0700

    week10: (task) implement search logic in repository

--- src/oop_67501_InosensiusDwiAtmoko/week10/WalletRepository.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week10/WalletRepository.kt
@@ -1,6 +1,7 @@
 package oop_67501_InosensiusDwiAtmoko.week10
 
 class WalletRepository<T : Any> {
+
     private val items = mutableListOf<T>()
 
     fun add(item: T) {
@@ -8,6 +9,13 @@ class WalletRepository<T : Any> {
     }
 
     fun getAll(): List<T> {
-        return items.toList()
+        return items
+    }
+
+    fun searchByName(query: String): List<T> {
+        return items.filter {
+            it is NamedEntity &&
+                    it.name.contains(query, ignoreCase = true)
+        }
     }
 }
