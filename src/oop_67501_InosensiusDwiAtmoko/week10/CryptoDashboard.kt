ceaa74c8884c55aab9caee3fc700b4bdfdea840e
INO22CEN <inocensiusdwi@gmail.com>
2026-05-08 13:44:55 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_67501_InosensiusDwiAtmoko/week10/CryptoDashboard.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week10/CryptoDashboard.kt
@@ -6,4 +6,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 0.75))
     coinRepo.add(Coin("ETH", 12.5))
     coinRepo.add(Coin("USDT", 1500.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
