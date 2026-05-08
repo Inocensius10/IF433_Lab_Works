d17b39ad80983e834abee4dd67a7857517436a13
INO22CEN <inocensiusdwi@gmail.com>
2026-05-08 13:44:26 +0700

    week10: (task) populate coin repository

--- src/oop_67501_InosensiusDwiAtmoko/week10/CryptoDashboard.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week10/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_67501_InosensiusDwiAtmoko.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    coinRepo.add(Coin("BTC", 0.75))
+    coinRepo.add(Coin("ETH", 12.5))
+    coinRepo.add(Coin("USDT", 1500.0))
 }
