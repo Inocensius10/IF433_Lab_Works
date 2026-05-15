2a741532364bc01e003e3720a0d5f9edb138d7a4
INO22CEN <inocensiusdwi@gmail.com>
2026-05-15 15:45:09 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_67501_InosensiusDwiAtmoko/week12/SmartFeeder.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week12/SmartFeeder.kt
@@ -17,4 +17,20 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
 
 fun main() {
     var currentKibbleStock = 50
+
+    println("=== JADWAL MAKAN PAGI ===")
+    try {
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+        println("Sisa stok setelah pagi: $currentKibbleStock gr")
+    } catch (e: DispenserJamException) {
+        println("Peringatan ke Pemilik: ${e.message}")
+    } catch (e: FoodEmptyException) {
+        println("Peringatan ke Pemilik: ${e.message}")
+    } catch (e: Exception) {
+        println("Peringatan ke Pemilik: ${e.message}")
+    }
 }
