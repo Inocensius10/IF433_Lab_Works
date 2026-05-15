ff19d0ccc1ab56162ca260af4cbcd8f331ffd9fc
INO22CEN <inocensiusdwi@gmail.com>
2026-05-15 15:32:26 +0700

    week12: implement throw keyword for validation

--- src/oop_67501_InosensiusDwiAtmoko/week12/CustomExceptions.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week12/CustomExceptions.kt
@@ -2,3 +2,18 @@ package oop_67501_InosensiusDwiAtmoko.week12
 
 class InsufficientFundsException(amount: Double, balance: Double) :
     Exception("Attempted $amount, balance: $balance")
+
+class BankAccount(var balance: Double) {
+    fun withdraw(amount: Double) {
+        if (amount < 0) {
+            throw IllegalArgumentException("Amount must be positive")
+        }
+
+        if (amount > balance) {
+            throw InsufficientFundsException(amount, balance)
+        }
+
+        balance -= amount
+        println("Withdrawal successful. Remaining balance: $balance")
+    }
+}
