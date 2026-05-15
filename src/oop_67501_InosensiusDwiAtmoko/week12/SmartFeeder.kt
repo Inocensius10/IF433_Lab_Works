3ea7c94d3812355fe6d407c3773deb31250b5dd2
INO22CEN <inocensiusdwi@gmail.com>
2026-05-15 15:49:44 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_67501_InosensiusDwiAtmoko/week12/SmartFeeder.kt
+++ src/oop_67501_InosensiusDwiAtmoko/week12/SmartFeeder.kt
@@ -50,4 +50,6 @@ fun main() {
         println("Peringatan ke Pemilik: ${error.message}")
         println("(Opsional: Berikan chicken jerky secara manual)")
     }
+
+    println("Pipeline Smart Feeder selesai. Stok akhir: $currentKibbleStock gr")
 }
