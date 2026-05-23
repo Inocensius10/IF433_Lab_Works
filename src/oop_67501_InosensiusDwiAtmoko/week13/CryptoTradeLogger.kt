5c0c5bb477c917414db98fe09f0e8c9d24c09306
INOCENSIU2203 <inocensiusdwi@gmail.com>
2026-05-24 01:48:46 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_67501_InosensiusDwiAtmoko/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_67501_InosensiusDwiAtmoko.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
